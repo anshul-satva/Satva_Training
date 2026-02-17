83fc9c9b9fab0e44542f3e754d4f17862924dc52
Anshul Panchal <anshulkumar@satvasolutions.com>
2026-02-17 18:11:54 +0530

    React Day 1 Task 2: The Lego Layout

---
+++ React/Day 1/Lego_layout/src/Components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Menu</h3>
+      <ul>
+        <li>Dashboard</li>
+        <li>Profile</li>
+        <li>Settings</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
