768f67cf673b8d5989dcbf9da33474d1343aae52
Anshul Panchal <anshulkumar@satvasolutions.com>
2026-03-02 09:25:21 +0530

    Basic Practicle of .NET in Console Application and Dependency injection practicle

---
+++ .NET Paybev/DI Practicle/Dependent_Project/DI/Greeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dependent_Project.DI
+{
+    public class Greeter : IGreeter
+    {
+        public string Greet(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
---
+++ .NET Paybev/DI Practicle/Dependent_Project/Services/ICalculatorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dependent_Project.Services
+{
+    public interface ICalculatorService
+    {
+        void Run();
+    }
+}
